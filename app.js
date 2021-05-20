75d43735101fd38f081ea7566745cd5cd70f6668
nadirbashir <1.nadirbashir@gmail.com>
2021-05-21 02:53:03 +0500

    10 Task complete

---
+++ app.js
@@ -0,0 +1,85 @@
+// /********Quest. 1 *********/
+
+// var rightNow = new Date();
+// document.getElementById("date").innerHTML = rightNow;
+
+// /********Quest. 2 *********/
+
+// var months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// var d = new Date();
+// document.getElementById("date").innerHTML = months[d.getMonth()];
+
+// /********Quest. 3/4 *********/
+
+// var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var d = new Date();
+// document.getElementById("date").innerHTML = "Today is " + days[d.getDay()];
+
+// var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var d = new Date();
+// if(d.getDay() === 0 || d.getDay() === 6){
+//     document.getElementById("date").innerHTML = "It's Fun day" + days[d.getDay()]
+// }else{
+//     document.getElementById("date").innerHTML = "It's " + days[d.getDay()]}
+
+// /********Quest. 5 *********/
+// var d = new Date();
+// d.setDate(d.getDate() - 10);
+// if(d.getDate() < 16){
+//     document.write("First Fifteen days of the month");
+// }else{
+//     document.write("Last days of the month");
+// }
+
+// /********Quest. 6 *********/
+
+// var d = new Date();
+// document.getElementById("date").innerHTML = "Current Date: " + d + "<br>Elapsed Milliseconds since January 1, 1970: " + d.getTime()+ "<br>Elapsed Minutes since January 1, 1970: " + d.getTime()/(1000*60);
+
+
+// /********Quest. 7 *********/
+
+// var d = new Date();
+// console.log(d.getHours())
+// if(d.getHours() <= 12){
+//     alert("It's AM")
+// }else{
+//     alert("it's PM")
+// }
+
+// /********Quest. 8 *********/
+
+// var d = new Date();
+// d.setFullYear(2021,11,31);
+// document.write("Later Date: " + d);
+
+// var d = new Date();
+// d.setDate(d.getDate() + 10);
+// console.log(d); 
+
+// var d = new Date();
+// d.setMonth(d.getMonth() - 3);
+// console.log(d);
+
+// /********Quest. 9 *********/
+// var d = new Date();
+// d.setFullYear(2021,3,14);
+// var currentDate = new Date();
+// days  = currentDate.getTime() - d.getTime();
+// document.write("Number of days passed since 1st Ramadan 2021: " + days/(1000*60*60*24));
+
+
+// /********Quest. 10 *********/
+
+// var d = new Date();
+// d.setFullYear(2021,0,1);
+// var currentDate = new Date();
+// document.write("On Reeference date" + currentDate + ", <br>" + (currentDate.getTime() - d.getTime()) /1000 + "seconds had passed since biginning of 2021");
+
+// /********Quest. 11 *********/
+
+
+
+// /********Quest. 1 *********/
+// /********Quest. 1 *********/
+// /********Quest. 1 *********/
